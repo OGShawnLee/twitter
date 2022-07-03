f9cd57211a89a44d540539e40685d50104f14715
Shawn Lee <changoloco.evg@gmail.com>
2022-07-03 15:57:13 -0500

    :sparkles: feat(ui): add mobile bottom navigation bar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,3 +3,35 @@
 </script>
 
 <slot />
+
+<nav class="fixed bottom-0 inset-x-0 h-15 | bg-zinc-800">
+	<ul class="max-w-xs h-full mx-auto | flex items-center justify-between | text-32px">
+		<li>
+			<a href="/home">
+				<i class="bx bxs-home-circle" />
+			</a>
+		</li>
+		<li>
+			<a href="/search">
+				<i class="bx bx-search" />
+			</a>
+		</li>
+		<li>
+			<a href="/notifications">
+				<i class="bx bx-bell" />
+			</a>
+		</li>
+		<li class="relative">
+			<a href="/notifications">
+				<i class="bx bx-envelope" />
+			</a>
+			<a
+				class="absolute -top-24 right-1/2 h-12 min-w-12 w-12 | grid place-content-center | bg-sky-500 rounded-full transform translate-x-1/2"
+				href="/compose/tweet"
+			>
+				<i class="bx bx-pen | text-2xl" />
+				<span class="sr-only">Tweet</span>
+			</a>
+		</li>
+	</ul>
+</nav>
