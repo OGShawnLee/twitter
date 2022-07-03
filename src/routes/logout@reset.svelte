a077dd145859069d79137177a84f2cf521237c12
Shawn Lee <changoloco.evg@gmail.com>
2022-07-03 16:05:37 -0500

    :sparkles: feat(route): add logout page

---
+++ src/routes/logout@reset.svelte
@@ -0,0 +1,20 @@
+<main class="min-h-screen | flex items-center justify-center">
+	<div class="max-w-xs p-7.5 | bg-zinc-800 rounded-2xl">
+		<i class="bx bxl-twitter | mb-4 | text-40px" />
+		<div class="flex flex-col gap-4">
+			<h1 class="text-2xl font-medium">Log out of Twitter?</h1>
+			<p class="text-zinc-400 leading-snug">
+				You can always log back in at any time. If you just want to switch accounts, you can do that
+				by adding an existing account
+			</p>
+		</div>
+		<div class="grid gap-3 | mt-6">
+			<button class="min-h-10.5 px-6 py-2 | bg-white rounded-full text-zinc-900 font-medium">
+				Log Out
+			</button>
+			<button class="min-h-10.5 px-6 py-2 | rounded-full border-2 border-white font-medium">
+				Cancel
+			</button>
+		</div>
+	</div>
+</main>
