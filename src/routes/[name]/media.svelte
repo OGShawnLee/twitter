8c6394ac05d58c4cca1f4efd44f4172eac4b501b
Shawn Lee <changoloco.evg@gmail.com>
2022-07-03 16:33:32 -0500

    :sparkles: feat(route): add user/media mobile placeholder page

---
+++ src/routes/[name]/media.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Tweet } from "$lib/components";
+</script>
+
+<div class="grid gap-12">
+	<Tweet imageURL="https://i.pinimg.com/564x/86/b8/17/86b817cd726c5108bc786645939296c4.jpg" />
+</div>
