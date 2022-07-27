8c504e32f9374bfebf95d7d1c08460a12b7212a3
Shawn Lee <changoloco.evg@gmail.com>
2022-07-27 15:39:59 -0500

    :wrench: chore(firebase): initialise firebase

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,16 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+	// SUPABASE
 	readonly VITE_SUPABASE_URL: string;
 	readonly VITE_SUPABASE_ANON_KEY: string;
+	// FIREBASE
+	readonly VITE_API_KEY: string;
+	readonly VITE_AUTH_DOMAIN: string;
+	readonly VITE_PROJECT_ID: string;
+	readonly VITE_STORAGE_BUCKET: string;
+	readonly VITE_MESSAGING_SENDER_ID: string;
+	readonly VITE_APP_ID: string;
 }
 
 interface ImportMeta {
---
+++ src/firebase/index.ts
@@ -0,0 +1,25 @@
+import type { FirebaseOptions } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+function getUniqueApp(settings: FirebaseOptions) {
+	return isInitialised() ? getApp() : initializeApp(settings);
+}
+
+function isInitialised() {
+	return getApps().length >= 1;
+}
+
+const app = getUniqueApp({
+	apiKey: import.meta.env.VITE_API_KEY,
+	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_ID
+});
+
+export default app;
+export const auth = getAuth(app);
+export const db = getFirestore(app);
