84fd147f729b6b8645c8040c760ff81e0b444e13
Shawn Lee <changoloco.evg@gmail.com>
2022-07-27 19:21:50 -0500

    :alien: fix(route): rename /user/lists get endpoint to GET

--- src/routes/[name]/lists.ts
+++ src/routes/[name]/lists.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
-export const get: RequestHandler = ({ params: { name } }) => {
+export const GET: RequestHandler = ({ params: { name } }) => {
 	return {
 		status: 200,
 		body: { name }
