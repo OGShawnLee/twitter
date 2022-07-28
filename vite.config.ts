362cc8c7d9a0f5586b382e103186c53f04e9dec1
Shawn Lee <changoloco.evg@gmail.com>
2022-07-27 21:52:10 -0500

    :wrench: chore: serve app on localhost:3000

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import windicss from "vite-plugin-windicss";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { resolve } from "path";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
 	plugins: [sveltekit(), windicss()],
@@ -10,5 +13,8 @@ export default defineConfig({
 			"@root": resolve("src"),
 			"@root/*": resolve("src/*")
 		}
+	},
+	server: {
+		port: 3000
 	}
 });
