cdc8eba69a62f6ff27cd3e44af43dd659afcb705
Shawn Lee <changoloco.evg@gmail.com>
2022-07-30 15:33:55 -0500

    :sparkles: feat(route): add /settings/your_twitter_data/account prototype mobile page

---
+++ src/routes/settings/your_twitter_data/account.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import "@root/styles/button-rounded.css";
+	import { Header, SettingsLink } from "$lib/components";
+	import { Navigable } from "malachite-ui/components";
+	import { user } from "@root/state";
+</script>
+
+<svelte:head>
+	<title>Account Information / Twitter</title>
+</svelte:head>
+
+<Header>
+	<button class="button-rounded size-12 background-zinc" on:click={() => history.back()}>
+		<i class="bx bxs-left-arrow-alt | text-2xl" />
+		<span class="sr-only">Go Back</span>
+	</button>
+	<div class="grid">
+		<div class="flex items-center gap-3">
+			<h1 class="text-xl font-medium">Account Information</h1>
+		</div>
+		<span class="text-xs text-zinc-500">@{$user?.document.displayName}</span>
+	</div>
+</Header>
+
+<main class="max-w-md w-full mx-auto mt-20 mb-24">
+	<Navigable vertical>
+		<SettingsLink text="Display Name" href="/display_name" icon="bx-at">
+			@{$user?.document.displayName}
+		</SettingsLink>
+		<SettingsLink text="Verified" href="/account/request_verification" icon="bxs-badge-check" />
+	</Navigable>
+</main>
