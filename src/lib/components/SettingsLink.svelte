38abb633c7a7a6cd9c8c6069b1bc67e86ed263ff
Shawn Lee <changoloco.evg@gmail.com>
2022-07-30 15:29:29 -0500

    :sparkles: feat(route): add /settings/account prototype mobile page

--- src/lib/components/SettingsLink.svelte
+++ src/lib/components/SettingsLink.svelte
@@ -4,16 +4,32 @@
 	export let href: string;
 	export let icon: string;
 	export let text: string;
+	export let isDanger = false;
 </script>
 
 <NavigableItem
 	as="a"
-	class="group px-6 py-3 | flex items-center justify-between | outline-none border-2 border-transparent hover:bg-zinc-800 focus:border-white"
+	class="group px-6 py-3 | flex items-center justify-between | outline-none border-2 border-transparent hover:bg-zinc-800 {isDanger
+		? 'focus:border-rose-500'
+		: 'focus:border-white'}"
 	href="/settings{href}"
 >
 	<div class="flex items-center gap-3">
-		<i class="bx {icon} text-2xl text-zinc-400 group-focus:text-white" />
-		<span class="text-sm font-medium"> {text} </span>
+		<i
+			class="bx {icon} text-2xl text-zinc-400 {isDanger
+				? 'group-focus:text-rose-500'
+				: 'group-focus:text-white'}"
+		/>
+		{#if $$slots.default}
+			<div class="grid">
+				<span class="text-sm font-medium"> {text} </span>
+				<span class="text-xs text-zinc-400">
+					<slot />
+				</span>
+			</div>
+		{:else}
+			<span class="text-sm font-medium"> {text} </span>
+		{/if}
 	</div>
 	<i class="bx bx-chevron-right text-2xl text-zinc-400 group-focus:text-white" />
 </NavigableItem>
---
+++ src/routes/settings/account.svelte
@@ -0,0 +1,46 @@
+<script>
+	import "@root/styles/button-rounded.css";
+	import { Header, SettingsLink } from "$lib/components";
+	import { Navigable } from "malachite-ui/components";
+	import { user } from "@root/state";
+</script>
+
+<svelte:head>
+	<title>Account / Twitter</title>
+</svelte:head>
+
+<Header>
+	<button class="button-rounded size-12 background-zinc" on:click={() => history.back()}>
+		<i class="bx bxs-left-arrow-alt | text-2xl" />
+		<span class="sr-only">Go Back</span>
+	</button>
+	<div class="grid">
+		<div class="flex items-center gap-3">
+			<h1 class="text-xl font-medium">Account</h1>
+		</div>
+		<span class="text-xs text-zinc-500">@{$user?.document.displayName}</span>
+	</div>
+</Header>
+
+<main class="max-w-md w-full mx-auto mt-20 mb-24">
+	<span class="px-6 | block | text-xs text-zinc-400">
+		See information about your account, download an archive of your data, or learn your account
+		deactivation options.
+	</span>
+	<div class="mt-3">
+		<Navigable vertical>
+			<SettingsLink text="Account Information" href="/your_twitter_data/account" icon="bx-user-pin">
+				See your account information like your phone number and email address.
+			</SettingsLink>
+			<SettingsLink text="Change Password" href="/password" icon="bx-key">
+				Change your password at any time.
+			</SettingsLink>
+			<SettingsLink text="Download and Archive" href="/download_your_data" icon="bx-download">
+				Get insights into the type of information stored for your account.
+			</SettingsLink>
+			<SettingsLink text="Deactivate Account" href="/deactivate" icon="bx-trash" isDanger>
+				Find out how you can deactivate your account.
+			</SettingsLink>
+		</Navigable>
+	</div>
+</main>
