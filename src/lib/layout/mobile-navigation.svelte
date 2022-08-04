7a931360003e98945e038d967ef4f4f3736cb9b4
Shawn Lee <changoloco.evg@gmail.com>
2022-08-04 13:35:59 -0500

    :bug: fix: dont show mobile-navigation is user is not logged in

--- src/lib/layout/mobile-navigation.svelte
+++ src/lib/layout/mobile-navigation.svelte
@@ -1,11 +1,15 @@
 <script>
+	import { isLoggedIn } from "@root/state/user";
+
 	let className = "justify-between";
 
 	export { className as class };
 </script>
 
-<nav class="fixed bottom-0 inset-x-0 h-15 z-8 | bg-zinc-800">
-	<ul class="max-w-md h-full w-full px-6 mx-auto | flex items-center {className} | text-32px">
-		<slot />
-	</ul>
-</nav>
+{#if $isLoggedIn}
+	<nav class="fixed bottom-0 inset-x-0 h-15 z-8 | bg-zinc-800">
+		<ul class="max-w-md h-full w-full px-6 mx-auto | flex items-center {className} | text-32px">
+			<slot />
+		</ul>
+	</nav>
+{/if}
--- src/state/user.ts
+++ src/state/user.ts
@@ -1,4 +1,5 @@
 import type { UserState } from "@root/types";
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 
 export const user = writable<UserState | null>(null);
+export const isLoggedIn = derived(user, (user) => user !== null);
