910b20538cc69fdcddabc2ec143af0696f1c6061
Shawn Lee <changoloco.evg@gmail.com>
2022-08-12 12:52:12 -0500

    :lipstick: feat(ui): add share Tweet menu options

--- src/lib/components/Tweet/Tweet.svelte
+++ src/lib/components/Tweet/Tweet.svelte
@@ -139,11 +139,45 @@
 					{isDisabled}
 				/>
 			</TweetButtonLike>
-			<TweetButton
-				icon="bx-upload"
-				buttonClass="hover:text-sky-500 focus:text-sky-500"
-				iconClass="after:bg-sky-900/30 group-focus:after:border-sky-300"
-			/>
+
+			<Menu let:isOpen let:button let:items>
+				<TweetButton
+					use={button}
+					icon="bx-upload"
+					buttonClass="hover:text-sky-500 focus:text-sky-500"
+					iconClass="after:bg-sky-900/30 group-focus:after:border-sky-300"
+				/>
+
+				{#if isOpen}
+					<div
+						class="fixed inset-0 z-20 | bg-zinc-800/70"
+						transition:fade|local={{ easing: cubicOut }}
+					/>
+				{/if}
+
+				<div
+					class="fixed inset-x-0 bottom-0 z-20 bg-zinc-900 | grid | outline-none text-white"
+					slot="items"
+					use:items
+					use:hideScrollbar
+					transition:fly|local={{ y: 250 }}
+				>
+					<TweetMenuItem icon="bx-envelope" text="Send via Direct Message" />
+					{#if $user}
+						<TweetMenuItem icon="bx-bookmark" text="Bookmark" />
+					{/if}
+					<TweetMenuItem icon="bx-link" text="Copy link to Tweet" />
+					<MenuItem
+						as="button"
+						class={{
+							base: "min-h-10.5 mx-6 my-4 px-6 py-2 | rounded-full border-2",
+							selected: { on: "border-white", off: "border-zinc-600" }
+						}}
+					>
+						Cancel
+					</MenuItem>
+				</div>
+			</Menu>
 		</div>
 	</div>
 </article>
