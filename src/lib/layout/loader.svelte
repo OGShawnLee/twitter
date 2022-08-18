d6f76fd4b7b6e0e6dcde47a37356ceb8db2d1c34
Shawn Lee <changoloco.evg@gmail.com>
2022-08-18 16:33:31 -0500

    :adhesive_bandage: fix: display default title while application is loading a restricted route

--- src/lib/layout/loader.svelte
+++ src/lib/layout/loader.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>Twitter</title>
+</svelte:head>
+
 <div class="fixed inset-0 | grid place-content-center | bg-zinc-900">
 	<h1 class="sr-only">Twitter</h1>
 	<i class="bx bxl-twitter | text-8xl text-sky-500 " />
